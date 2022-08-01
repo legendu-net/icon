a77173c80c24aeeba5ef23bd12ab36fe1edf41ae
Benjamin Du <longendu@yahoo.com>
2022-08-01 01:06:48 -0700

    support icon update

---
+++ cmd/update.go
@@ -0,0 +1,25 @@
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+	"legendu.net/icon/utils"
+)
+
+// Update icon.
+func update(cmd *cobra.Command, args []string) {
+	command := utils.Format("curl -sSL https://raw.githubusercontent.com/legendu-net/icon/main/install_icon.sh | {prefix} bash -", map[string]string{
+		"prefix": utils.GetCommandPrefix(true, map[string]uint32{}),
+	})
+	utils.RunCmd(command)
+}
+
+var updateCmd = &cobra.Command{
+	Use:     "update",
+	Aliases: []string{"upd"},
+	Short:   "Update icon",
+	Run:     update,
+}
+
+func init() {
+	rootCmd.AddCommand(updateCmd)
+}
