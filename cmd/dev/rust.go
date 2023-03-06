f89226d706027aaa7419900e967a6f5b4441279f
Ben Du <longendu@yahoo.com>
2023-03-05 23:18:23 -0800

    fix issues in rust support

--- cmd/dev/rust.go
+++ cmd/dev/rust.go
@@ -68,7 +68,7 @@ func installSccache() {
 	defer os.RemoveAll(tmpdir)
 	file := filepath.Join(tmpdir, "sccache.tar.gz")
 	network.DownloadGitHubRelease("mozilla/sccache", "", []string{"x86_64", "unknown", "linux", "musl", "tar.gz"}, []string{"pre", "dist", "sha256"}, file)
-	command := utils.Format("{prefix} tar --strip-components=1 -C /usr/local/bin/ -zxvf {file} */sccache", map[string]string{
+	command := utils.Format("{prefix} tar --wildcards --strip-components=1 -C /usr/local/bin/ -zxvf {file} */sccache", map[string]string{
 		"prefix": utils.GetCommandPrefix(false, map[string]uint32{
 			"/usr/local/bin": unix.W_OK | unix.R_OK,
 		}),
